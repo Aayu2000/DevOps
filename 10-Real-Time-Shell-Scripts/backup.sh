04015bd60d6597b37fc178d3c640c770ba32134c
Aayu2000 <116865109+Aayu2000@users.noreply.github.com>
2024-05-23 21:40:15 +0530

    Update backup.sh
    Added script description in the recently pushed scripts

--- 10-Real-Time-Shell-Scripts/backup.sh
+++ 10-Real-Time-Shell-Scripts/backup.sh
@@ -1,8 +1,29 @@
-#!/bin/bash 
-SOURCE="/home/ubuntu/Source/"
-DESTINATION="/home/ubuntu/Destination/"
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
-# Create backup directory and copy files 
-mkdir -p $DESTINATION/$DATE |
-cp -r $SOURCE $DESTINATION/$DATE
-echo "Backup completed on $DATE" 
+#!/bin/bash
+###############################
+# Author: Aayush
+# Date: 14/05/2024
+# Version: V1
+# This script will list he usernames of github users who has access to the specified repository
+#
+# Usage - call the script it will create a backup of the specified source file or folder
+###############################
+#!/bin/bash
+# Source directory to back up
+SOURCE_DIR="/home/ubuntu/Source/"
+
+# Destination directory where backups will be stored
+DEST_DIR="/home/ubuntu/Destination/"
+
+# Create a timestamp
+TIMESTAMP=$(date +"%d-%m-%Y_%H-%M-%S")
+
+# Create the destination directory with the timestamp
+BACKUP_DIR="${DEST_DIR}/backup_${TIMESTAMP}"
+mkdir -p "${BACKUP_DIR}"
+
+# Perform the backup using rsync
+rsync -av --delete "${SOURCE_DIR}/" "${BACKUP_DIR}/"
+
+# Print a message indicating the backup is complete
+echo "Backup of ${SOURCE_DIR} completed at ${TIMESTAMP} and stored in ${BACKUP_DIR}"
+
