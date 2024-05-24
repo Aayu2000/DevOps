f69697df517cbf6dbb4686e1eda2791e2175a092
Aayu2000 <upadhyayaayush989@gmail.com>
2024-05-24 08:34:44 +0000

    check-system-uptime.sh added

---
+++ 10-Real-Time-Shell-Scripts/system-uptime-check.sh
@@ -0,0 +1,20 @@
+###############################
+# Author: Aayush
+# Date: 24/05/2024
+# Version: V1
+# This script will check the system uptime
+#
+# Usage - call the script it will show the system uptime
+###############################
+
+#!/bin/bash
+
+# Get the current date and time
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Get the system uptime
+UPTIME=$(uptime -p)
+
+# Print the timestamp and uptime to the terminal
+echo "[$TIMESTAMP] System Uptime: $UPTIME"
+
